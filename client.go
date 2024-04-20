75a2c4bb1830412ea08f4ecf1f54226a87b187ed
Yuvraj <142736361+tqindia@users.noreply.github.com>
2024-04-20 12:40:43 +0530

    more

--- client.go
+++ client.go
@@ -7,25 +7,6 @@ import (
 )
 
 
-// Add Logging, You can use any lib
-//TODO: Add code docs
-// Once we are done with mysql and postgres, Let's rethink about the config structure
-type Config struct {
-	Host         string `yaml:"host" pflag:",Database host url"`
-	Username     string `yaml:"username" pflag:",Database username"`
-	Password     string `yaml:"password" pflag:",Database password"`
-	DatabaseName string `yaml:"database" pflag:",Database name"`
-	Port         string `yaml:"port" pflag:",Database Port"`
-	SSL          string `yaml:"ssl" pflag:",Database ssl enable/disable"`
-	ProjectID    string `yaml:"project_id" pflag:",BigQuery project ID"`
-	JSONKeyPath  string `yaml:"json_key_path" pflag:",Path to BigQuery JSON key file"`
-	DBType       string `yaml:"type" pflag:",Database type"`
-	Warehouse string         `yaml:"warehouse" pflag:",Snowflake warehouse"`
-	Schema    string         `yaml:"schema" pflag:",Snowflake database schema"`
-	Account   string         `yaml:"account" pflag:",Snowflake account ID"`
-	Debug     bool           `yaml:"debug" pflag:",Debug mode"`
-}
-
 //TODO: Add description
 // Please fix the interface changes
 type ISQL interface {
@@ -35,12 +16,12 @@ type ISQL interface {
 }
 
 //TODO: Add description
-func NewClient(dbConfig *Config, dbType DbType) (ISQL, error)  {
+func NewClientWithConfig(dbConfig *config.Config, dbType DbType) (ISQL, error)  {
 		switch dbType {
 		case MySQL:
-			return mysql.NewMySQL(dbConfig)
+			return mysql.NewMySQLWithConfig(dbConfig)
 		case Postgres:
-			return postgres.NewPostgres(dbConfig)
+			return postgres.NewPostgresWithConfig(dbConfig)
 		// case "snowflake":
 		// 	return &SnowFlake{},nil
 		// case "bigquery":
@@ -50,4 +31,21 @@ func NewClient(dbConfig *Config, dbType DbType) (ISQL, error)  {
 		default:
 			return nil, fmt.Errorf("unsupported database type: %s", dbType)
 	 }
+}
+
+func NewClient(dbClient *sql.DB, dbType DbType) (ISQL, error)  {
+	switch dbType {
+	case MySQL:
+		return mysql.NewMySQL(dbClient)
+	case Postgres:
+		return postgres.NewPostgres(dbClient)
+	// case "snowflake":
+	// 	return &SnowFlake{},nil
+	// case "bigquery":
+	// 	return &Bigquery{},nil
+	// case "redshift":
+	// 	return &RedShift{},nil
+	default:
+		return nil, fmt.Errorf("unsupported database type: %s", dbType)
+ }
 }
