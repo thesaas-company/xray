dbe1a62d1c927107d26592cdc633b72355043d9b
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-04-30 14:04:27 +0530

    added test cases for bigquery
    Signed-off-by: adarsh-jaiss <its.adarshjaiss@gmail.com>

--- example/snowflake/main.go
+++ example/snowflake/main.go
@@ -9,12 +9,14 @@ import (
 	"github.com/thesaas-company/xray/types"
 )
 
+// export DB Passowrd, Export root=DB_PASSWORD
 func main() {
 	config := &config.Config{
-		Account:  "account",
-		Username : "root",
-		DatabaseName: "employees",
-		Warehouse: "Datasherlock",
+		Account:      "https://tvhcdje-pd56667.snowflakecomputing.com",
+		Username:     "jaizadarsh",
+		DatabaseName: "TEST",
+		Warehouse:    "TEST",
+		
 	}
 
 	client, err := xray.NewClientWithConfig(config, types.Snowflake)
